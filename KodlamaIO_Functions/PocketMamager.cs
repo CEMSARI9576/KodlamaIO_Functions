05ff3166c1fd1ea0308ab309df7873fea53337d5
cmsr9576 <cmsr9576@gmail.com>
2021-01-16 23:42:03 +0300

    Proje dosyası ekle.

---
+++ KodlamaIO_Functions/PocketMamager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KodlamaIO_Functions
+{
+    class PocketMamager
+    {
+        public  void Ekle(Product urun)
+        {
+            Console.WriteLine("Sepete Eklendi :"+urun.Adi);
+        }
+            
+        public void Ekle2(string urunadi,string aciklama,int fiyati,int stokadedi)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Sepete Eklendi"+urunadi+aciklama+stokadedi);
+
+        }
+    }
+}
---
+++ KodlamaIO_Functions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KodlamaIO_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+            //Type Safe
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------------");
+
+
+            }
+            Console.WriteLine("-----------------");
+
+            PocketMamager Ekle = new PocketMamager();
+            Ekle.Ekle(urun1);
+            Ekle.Ekle(urun2);
+        
+            Ekle.Ekle2("Armut", "yESİL", 12,10);
+            Ekle.Ekle2("Elma", "Kırmızı", 15,56);
+
+
+
+            
+          
+        }
+    }
+}
+//Do not repeat yourself-DRY-Clean Code-Best Practice
